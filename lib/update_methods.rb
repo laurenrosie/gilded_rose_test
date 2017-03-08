dc40e2de714f2a7aee8cdeadc1bafbb90e0791a6
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-03-08 18:49:53 +0000

    Extract magic numbers from update methods

--- lib/update_methods.rb
+++ lib/update_methods.rb
@@ -1,17 +1,22 @@
 module UpdateMethods
 
+    MIN_QUALITY = 0
+    MAX_QUALITY = 50
+    MIN_SELL_IN = 0
+    DEFAULT_QUALITY_CHANGE = 1
+    DEFAULT_SELL_IN_CHANGE = 1
 
     def update_normal(item)
-      if item.sell_in > 0
+      if item.sell_in > MIN_SELL_IN
         decrease_sellin(item)
-        decrease_quality(item) if item.quality > 0
+        decrease_quality(item) if item.quality > MIN_QUALITY
       else
-        2.times{decrease_quality(item) if item.quality > 0}
+        2.times{decrease_quality(item) if item.quality > MIN_QUALITY}
       end
     end
 
     def update_aged(item)
-      if item.sell_in > 0
+      if item.sell_in > MIN_SELL_IN
         decrease_sellin(item)
         increase_quality(item) if below_max_quality(item)
       else
@@ -26,7 +31,7 @@ module UpdateMethods
       elsif item.sell_in > 5
         decrease_sellin(item)
         2.times{increase_quality(item) if below_max_quality(item)}
-      elsif item.sell_in > 0
+      elsif item.sell_in > MIN_SELL_IN
         decrease_sellin(item)
         3.times{increase_quality(item) if below_max_quality(item)}
       else
@@ -38,28 +43,28 @@ module UpdateMethods
     end
 
     def update_conjured(item)
-      if item.sell_in > 0
+      if item.sell_in > MIN_SELL_IN
         decrease_sellin(item)
-        2.times{decrease_quality(item) if item.quality > 0}
+        2.times{decrease_quality(item) if item.quality > MIN_QUALITY}
       else
-        4.times{decrease_quality(item) if item.quality > 0}
+        4.times{decrease_quality(item) if item.quality > MIN_QUALITY}
       end
     end
 
     def increase_quality(item)
-      item.quality = item.quality + 1
+      item.quality = item.quality + DEFAULT_QUALITY_CHANGE
     end
 
     def decrease_quality(item)
-      item.quality = item.quality - 1
+      item.quality = item.quality - DEFAULT_QUALITY_CHANGE
     end
 
     def decrease_sellin(item)
-      item.sell_in = item.sell_in - 1
+      item.sell_in = item.sell_in - DEFAULT_SELL_IN_CHANGE
     end
 
     def below_max_quality(item)
-      item.quality < 50
+      item.quality < MAX_QUALITY
     end
 
 end
