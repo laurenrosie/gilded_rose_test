6ae7e41fb4ce159d1b609ab9bcaaaed7a357a0a9
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-03-30 11:43:09 +0100

    Refactor Gilded Rose methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,18 +12,15 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if is_regular?(item)
-        update_normal(item)
-      elsif is_aged?(item)
-        update_aged(item)
-      elsif is_passes?(item)
-        update_passes(item)
-      elsif is_conjured?(item)
-        update_conjured(item)
-      else
-        update_legendary(item)
-      end
+      update_item(item)
     end
   end
 
+  def update_item(item)
+    update_normal(item) if is_regular?(item)
+    update_aged(item) if is_aged?(item)
+    update_passes(item) if is_passes?(item)
+    update_conjured(item) if is_conjured?(item)
+    update_legendary(item) if is_legendary?(item)
+  end
 end
--- lib/item_update_methods.rb
+++ lib/item_update_methods.rb
@@ -5,50 +5,57 @@ module ItemUpdateMethods
     MIN_SELL_IN = 0
     DEFAULT_QUALITY_CHANGE = 1
     DEFAULT_SELL_IN_CHANGE = 1
+    NORMAL_QUALITY_INCREASE_MULTIPLE = 2
+    PASSES_QUALITY_INCREASE_MULTIPLE = 3
+    CONJURED_QUALITY_INCREASE_MULTIPLE = 4
 
     def update_normal(item)
-      if item.sell_in > MIN_SELL_IN
+      if above_min_sell_in(item)
         decrease_sellin(item)
-        decrease_quality(item) if item.quality > MIN_QUALITY
+        decrease_quality(item) if above_min_quality(item)
       else
-        2.times{decrease_quality(item) if item.quality > MIN_QUALITY}
+        NORMAL_QUALITY_INCREASE_MULTIPLE.times{decrease_quality(item) if above_min_quality(item)}
       end
     end
 
     def update_aged(item)
-      if item.sell_in > MIN_SELL_IN
+      if above_min_sell_in(item)
         decrease_sellin(item)
         increase_quality(item) if below_max_quality(item)
       else
-        2.times{increase_quality(item) if below_max_quality(item)}
-      end
-    end
-
-    def update_passes(item)
-      if item.sell_in >10
-        decrease_sellin(item)
-        increase_quality(item) if below_max_quality(item)
-      elsif item.sell_in > 5
-        decrease_sellin(item)
-        2.times{increase_quality(item) if below_max_quality(item)}
-      elsif item.sell_in > MIN_SELL_IN
-        decrease_sellin(item)
-        3.times{increase_quality(item) if below_max_quality(item)}
-      else
-        item.quality = 0
+        NORMAL_QUALITY_INCREASE_MULTIPLE.times{increase_quality(item) if below_max_quality(item)}
       end
     end
 
     def update_legendary(item)
     end
 
-    def update_conjured(item)
-      if item.sell_in > MIN_SELL_IN
+    def update_passes(item)
+      if item.sell_in >10
+        in_date_item_update(item)
+      elsif item.sell_in > 5
         decrease_sellin(item)
-        2.times{decrease_quality(item) if item.quality > MIN_QUALITY}
+        NORMAL_QUALITY_INCREASE_MULTIPLE.times{increase_quality(item) if below_max_quality(item)}
+      elsif item.sell_in > MIN_SELL_IN
+        decrease_sellin(item)
+        PASSES_QUALITY_INCREASE_MULTIPLE.times{increase_quality(item) if below_max_quality(item)}
       else
-        4.times{decrease_quality(item) if item.quality > MIN_QUALITY}
+        item.quality = 0
       end
+    end
+
+    def update_conjured(item)
+      if above_min_sell_in(item)
+        decrease_sellin(item)
+        NORMAL_QUALITY_INCREASE_MULTIPLE.times{decrease_quality(item) if item.quality > MIN_QUALITY}
+      else
+        CONJURED_QUALITY_INCREASE_MULTIPLE.times{decrease_quality(item) if item.quality > MIN_QUALITY}
+      end
+    end
+
+    def in_date_item_update(item)
+      decrease_sellin(item)
+      increase_quality(item) if below_max_quality(item)
     end
 
     def increase_quality(item)
@@ -65,6 +72,14 @@ module ItemUpdateMethods
 
     def below_max_quality(item)
       item.quality < MAX_QUALITY
+    end
+
+    def above_min_quality(item)
+      item.quality > MIN_QUALITY
+    end
+
+    def above_min_sell_in(item)
+      item.sell_in > MIN_SELL_IN
     end
 
 end
